b20880eae5a46df2a49a8108e8006626c07363f9
Thadah Denyse <thadahdenyse@gmail.com>
2017-10-25 16:27:54 +0200

    MPD Scriptak aldatuta
    Streaming egiteko MPD script bat sortua

--- zerbitzaria/hasistreaming.sh
+++ zerbitzaria/hasistreaming.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 ffmpeg -re -i /dev/video0 -f alsa -i hw:1 -acodec libvorbis -g 52 -b:a 64k -r 30 -ar 44100 -vcodec libx264 -s 640x480 -preset ultrafast -f mp4 -movflags frag_keyframe+empty_moov stream.mp4
+rm stream.mp4
+exit
--- zerbitzaria/mpdsortu.sh
+++ zerbitzaria/mpdsortu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-MP4Box -dash 10000 -dash-profile dashavc265:live -segment-name output-seg output.mp4
+MP4Box -dash 10000 -dash-profile 'dashavc265:live' -segment-name vod/vod_ output.mp4

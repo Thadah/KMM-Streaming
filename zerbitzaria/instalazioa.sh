e1f37fe7a1a6ddb3822ea87a0801438ad84ded84
jonArzelus <kontrabass32@hotmail.com>
2017-11-08 17:08:26 +0100

    aldaketak instalazio fitxategian

--- zerbitzaria/instalazioa.sh
+++ zerbitzaria/instalazioa.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-sudo apt install -y  apache2 ffmpeg mp4box x264 #aldatzeko
-sudo npm install -g grunt-cli
-sudo apt install -y subversion
-svn co https://svn.code.sf.net/p/gpac/code/trunk/gpac gpac
+sudo apt install -y  apache2 ffmpeg subversion x264 zlib1g-dev #programak, subversion mp4box instalatzeko
+svn co https://svn.code.sf.net/p/gpac/code/trunk/gpac gpac #mp4box
 cd gpac
 sudo chmod +x configure
 ./configure
 sudo make
 sudo make install
 sudo cp bin/gcc/libgpac.so /usr/lib
-
+mp4box -version
+cd ..
+sudo rm -r gpac
 echo " "
-echo "Bukatu da instalazioa, egin 'MP4Box -version' ondo instalatu dela segurtatzeko"
+echo "Bukatu da instalazioa"
 exit

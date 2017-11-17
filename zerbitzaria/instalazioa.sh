1f41de6d2f29b4d03177c087e402c40087595f3b
Thadah Denyse <thadahdenyse@gmail.com>
2017-11-17 16:08:41 +0100

    Node paketea jada ez da beharrezkoa

--- zerbitzaria/instalazioa.sh
+++ zerbitzaria/instalazioa.sh
@@ -3,8 +3,7 @@ echo "Apache, MP4Box, DashCast eta X264 instalatzen..."
 sudo apt install -y gpac gcc make zlib1g-dev
 sudo add-apt-repository ppa:jonathonf/ffmpeg-3 -y
 sudo apt update
-sudo apt install -y --install-recommends apache2 ffmpeg x264 #aldatzeko
-sudo npm install -g grunt-cli
+sudo apt install -y --install-recommends apache2 ffmpeg x264
 git clone https://github.com/gpac/gpac.git
 cd gpac
 ./configure

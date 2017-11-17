1cb776ee4869a09b1fea97ba4cee52c47fb2d2dd
jonArzelus <kontrabass32@hotmail.com>
2017-11-17 12:53:22 +0100

    instalazioa eta bezeroa

--- zerbitzaria/instalazioa.sh
+++ zerbitzaria/instalazioa.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Apache, MP4Box, DashCast eta X264 instalatzen..."
+sudo apt install -y gpac gcc make zlib1g-dev
 sudo add-apt-repository ppa:jonathonf/ffmpeg-3 -y
 sudo apt update
 sudo apt install -y --install-recommends apache2 ffmpeg x264 #aldatzeko
